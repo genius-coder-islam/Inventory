a1d58dae1d224bb68798a01228dbf1ad2717bf40
genius-coder-islam <mr.islammansour@gmail.com>
2026-03-14 20:45:19 +0200

    Starting Practical Excercise - Inventory project 14-3-2026

---
+++ Inventory.Models/Bill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Bill
+    {
+
+        public int BillId { get; set; } 
+
+    }
+}
